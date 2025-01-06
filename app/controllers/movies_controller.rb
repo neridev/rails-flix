ebe7118f6f5c2afe2acb77d3335a75b86c79e8ab
Peter Bubelíny <peter@bubeliny.sk>
2025-01-06 16:03:03 +0100

    Forms: creating records

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,18 @@ class MoviesController < ApplicationController
         @movies = Movie.all
     end
 
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+
+        @movie.save
+
+        redirect_to @movie
+    end
+
     def show
         @movie = Movie.find(params[:id])
     end
@@ -15,10 +27,15 @@ class MoviesController < ApplicationController
     def update
         @movie = Movie.find(params[:id])
 
-        movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
-
         @movie.update(movie_params)
 
         redirect_to @movie
     end
+
+    private
+
+        def movie_params
+            params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+        end
+
 end
